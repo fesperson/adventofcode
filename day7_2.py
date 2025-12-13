ecf5ae47f5d808db89c74402c20a0d47167e1708
Felix Esperson <felix.esperson@gmail.com>
2025-12-13 18:04:05 +1100

    day 7 part 1 completed

---
+++ day7_2.py
@@ -0,0 +1,28 @@
+import numpy as np
+with open("day_7_example.txt", "r") as file:
+    content = file.read()
+    arr = content.split("\n")
+grid = []
+for row in arr:
+    grid.append(list(row))
+beam_locs = np.full(len(grid[0]), ".", dtype=str)
+print(beam_locs)
+print(grid)
+splits = 0
+
+
+for i, element in enumerate(grid[0]):
+    if element == "S":
+        beam_locs[i] = "|"
+
+for row in grid:
+    for i, col in enumerate(row[1:]):
+        if row[i] == "|" and  beam_locs[i] == "|":
+            beam_locs[i] = "|"
+        elif row[i] == "^" and beam_locs[i] == "|":
+            beam_locs[i+1] = "|"
+            beam_locs[i-1] = "|"
+            beam_locs[i] = "."
+            splits += 1
+    print(beam_locs)
+print(splits)
